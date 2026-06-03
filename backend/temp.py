c2c88754d2e5f8ce8e0cb3f17be8464eb31f886f
tsingh2 <tsingh2@blueprintmedicines.com>
2026-06-03 13:34:22 -0400

    moved requirements.txt to backend

--- backend/temp.py
+++
@@ -1,9 +0,0 @@
-from jose import JWTError, jwt
-
-secret = "mysecret"
-data = {"sub": "123"}
-
-token = jwt.encode(data, secret, algorithm="HS256")
-decoded = jwt.decode(token, secret, algorithms=["HS256"])
-
-print(decoded)
